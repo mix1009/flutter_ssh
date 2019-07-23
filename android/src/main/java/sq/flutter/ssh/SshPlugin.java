fcee36885760b042a55d28de4339deea076da69d
mix1009 <mix1009@gmail.com>
2019-07-23 11:17:25 +0900

    fix Android crash issue "Methods marked with @UiThread must be executed on the main thread." on Flutter stable channel(1.7.8) reference: https://github.com/flutter/flutter/issues/34993

--- android/src/main/java/sq/flutter/ssh/SshPlugin.java
+++ android/src/main/java/sq/flutter/ssh/SshPlugin.java
@@ -28,6 +28,9 @@ import java.util.Vector;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.EventChannel.EventSink;
 import io.flutter.plugin.common.EventChannel.StreamHandler;
@@ -48,8 +51,54 @@ public class SshPlugin implements MethodCallHandler, StreamHandler {
     eventChannel.setStreamHandler(instance);
   }
 
+  // MethodChannel.Result wrapper that responds on the platform thread.
+  private static class MethodResultWrapper implements Result {
+    private Result methodResult;
+    private Handler handler;
+
+    MethodResultWrapper(Result result) {
+      methodResult = result;
+      handler = new Handler(Looper.getMainLooper());
+    }
+
+    @Override
+    public void success(final Object result) {
+      handler.post(
+          new Runnable() {
+            @Override
+            public void run() {
+              methodResult.success(result);
+            }
+          });
+    }
+
+    @Override
+    public void error(
+        final String errorCode, final String errorMessage, final Object errorDetails) {
+      handler.post(
+          new Runnable() {
+            @Override
+            public void run() {
+              methodResult.error(errorCode, errorMessage, errorDetails);
+            }
+          });
+    }
+
+    @Override
+    public void notImplemented() {
+      handler.post(
+          new Runnable() {
+            @Override
+            public void run() {
+              methodResult.notImplemented();
+            }
+          });
+    }
+  }  
+
   @Override
-  public void onMethodCall(MethodCall call, Result result) {
+  public void onMethodCall(MethodCall call, Result rawResult) {
+    Result result = new MethodResultWrapper(rawResult);
     if (call.method.equals("connectToHost")) {
       connectToHost((HashMap) call.arguments, result);
     } else if (call.method.equals("execute")) {
